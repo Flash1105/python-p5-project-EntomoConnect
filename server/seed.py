f6ed72fa9516395949bebd8a5f5c65017b66e658
Flash1105-2 <flashpay110599@gmail.com>
2023-10-05 19:39:08 -0400

    create preset data

--- server/seed.py
+++ server/seed.py
@@ -3,10 +3,16 @@ from extensions import db
 from models import User, Observation, Discussion
 
 def add_users():
+    user1 = User(username='user1', email='user1', password='user1')
+    user2 = User(username='user2', email='user2', password='user2')
 
-    pass
+    db.session.add(user1)
+    db.session.add(user2)
+    db.session.commit()
+    
 def add_observations():
     observation1 = Observation(title='Beautiful Butterfly', content= 'Today, I spotted a Monarch Butterfly.')
+    observation2= Observation(title='Orange Locus', content= 'Today, I spotted an Orange Locus.')
     db.session.add(observation1)
     db.session.commit()
 
