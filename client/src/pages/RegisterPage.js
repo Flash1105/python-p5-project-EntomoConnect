ce653085fdbd6cc1334f9b8fbf789ebbc60ade19
Flash1105-2 <flashpay110599@gmail.com>
2023-10-04 21:35:51 -0400

    correct small typo errors

--- client/src/pages/RegisterPage.js
+++ client/src/pages/RegisterPage.js
@@ -1,13 +1,52 @@
 import React from 'react'
+import { useFormik } from 'formik'
 
-
-const RegisterPage = () => {
-    return (
-        <div>
-            <h1>Register</h1>
-            {/* Registration form */}
-        </div>
-    );
+const validate = values => {
+    const errors = {};
+    if (!values.username) {
+        errors.username = 'Required';
+    }
+    if (!values.email) {
+        errors.email = 'Required';
+    }
+    if (!values.password) {
+        errors.password = 'Required';
+    }
+    if (values.password !== values.confirmPassword) {
+        errors.confirmPassword = "Password must match"
+    }
+    return errors;
 };
 
-export default RegisterPage
+const RegisterPage = () => {
+    const formik = useFormik({
+        initialValues: {
+            username: '',
+            email: '',
+            password: '',
+            confirmPassword:''
+        },
+        validate,
+        onSubmit: values => {
+            console.log(values);
+        },
+});
+
+    return (
+        <form onSubmit={formik.handleSubmit}>
+            <label htmlFor="username">Username</label>
+            <input
+                id="username"
+                name="username"
+                type="text"
+                onChange={formik.handleChange}
+                value={formik.values.username}
+            />
+            {formik.errors.username ? <div>{formik.errors.username}</div> : null}
+
+            <button type="submit">Submit</button>
+        </form>
+    );
+    };
+
+    export default RegisterPage
