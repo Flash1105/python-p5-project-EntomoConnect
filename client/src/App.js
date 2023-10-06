dcca230d83da21e6ac19a76ca09442ec070d9e1e
Flash1105-2 <flashpay110599@gmail.com>
2023-10-06 13:44:09 -0400

    change prioority

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
+import Dashbaord from './pages/DashboardPage';
 import RegisterPage from './pages/RegisterPage';
 import LoginPage from './pages/LoginPage';
 
@@ -12,6 +13,7 @@ const App = () => {
       <Route path="/" exact component={HomePage} />
       <Route path="/register" component={RegisterPage} />
       <Route path="/login" component={LoginPage} />
+      <Route path="/dashboard" component={Dashbaord} />
     </Router>
   );
 };
