7b610361916c0736b4b34adcb80343bab804ebbe
Flash1105-2 <flashpay110599@gmail.com>
2023-10-05 21:57:34 -0400

    add key constraint

--- server/models.py
+++ server/models.py
@@ -1,16 +1,10 @@
-from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime 
 from extensions import db
-from sqlalchemy import ForeignKey
+from sqlalchemy import ForeignKey, PrimaryKeyConstraint
 from sqlalchemy.orm import relationship
 
 ############
-likes = db.Table(
-    'likes',
-    db.Column('user_id', db.Integer, db.ForeignKey('users.id'), primary_key=True),
-    db.Column('observation_id', db.Integer, db.ForeignKey('observations.id'), primary_key=True),
-    extend_existing=True
-)
+
 
 class Observation(db.Model):
     __tablename__ = 'observations'
@@ -45,7 +39,6 @@ class User (db.Model):
 
 class Like (db.Model):
     __tablename__='likes'
-    id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     discussion_id = db.Column(db.Integer, db.ForeignKey('discussions.id'), nullable=True)
     observation_id = db.Column(db.Integer, db.ForeignKey('observations.id'), nullable=True)
@@ -53,7 +46,10 @@ class Like (db.Model):
     user = db.relationship('User', back_populates='likes')
     discussion = db.relationship('Discussion', back_populates='likes')
     observation = db.relationship('Observation', back_populates='likes')
-
+    __table_args__ = (
+        PrimaryKeyConstraint('user_id', 'discussion_id', 'observation_id'),
+        {}
+    )
 
     def __repr__(self):
-        return f'<User {self.username}>'
+        return f'<User {self.id}>'
