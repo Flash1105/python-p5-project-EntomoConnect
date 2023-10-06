d2ae9056598b4ed74bbb039ad9f2e1a034f79b00
Flash1105-2 <flashpay110599@gmail.com>
2023-10-05 21:57:51 -0400

    def register with route

--- server/app.py
+++ server/app.py
@@ -1,11 +1,12 @@
-from flask import Flask
+from flask import Flask, request, jsonify, render_template
 from config import Config
 from flask_migrate import Migrate
 from extensions import db
+from models import User
 
 #########################
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='client/src/pages')
 app.config.from_object(Config)
 
 db.init_app(app)
@@ -13,7 +14,31 @@ migrate = Migrate(app, db)
 
 @app.route('/')
 def home():
-    return '<h1>Project Server</h1>'
+    return render_template('index.html')
+
+
+@app.route('/api/register', methods = ['POST'])
+def register():
+    data = request.json 
+
+    username = data.get('username')
+    email = data.get('email')
+    password = data.get('password')
+
+    existing_user = User.query.filter_by(username=username).first()
+    existing_email = User.query.filter_by(email=email).first()
+
+    if existing_user:
+        return jsonify({'error': 'Username already exists'}), 400
+    if existing_email:
+        return jsonify({'error': 'Email already exists'}), 400
+    
+    new_user = User(username=username, email=email, password=password)
+
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify({'message': 'User created successfully'}), 201
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
