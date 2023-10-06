2217506df02a369476a66dc9e6a713dbba6f6881
Flash1105-2 <flashpay110599@gmail.com>
2023-10-06 02:20:25 -0400

    correct relationship typos

--- server/models.py
+++ server/models.py
@@ -7,41 +7,43 @@ from sqlalchemy.orm import relationship
 
 
 class Observation(db.Model):
-    __tablename__ = 'observations'
+    __tablename__ = 'observation'
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(255), nullable=False)
     content = db.Column(db.Text, nullable=False)
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-    user = db.relationship("User", back_populates="observations")
-    likes = db.relationship('Like', back_populates='observations')
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user = db.relationship("User", back_populates="observations", primaryjoin= lambda: User.id == Observation.user_id)
+    likes = db.relationship('Like', back_populates='observation')
 
 class Discussion(db.Model):
-    __tablename__ = 'discussions'
+    __tablename__ = 'discussion'
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.Text, nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
-    user_id = db.Column(db.Integer, ForeignKey('users.id'), nullable=False)
-    observation_id = db.Column(db.Integer, ForeignKey('observations.id'), nullable=False)
-    user = db.relationship("User", back_populates="discussions")
-    likes = db.relationship('Like', back_populates='discussions')
+    user_id = db.Column(db.Integer, ForeignKey('user.id'), nullable=False)
+    observation_id = db.Column(db.Integer, ForeignKey('observation.id'), nullable=False)
+    user = db.relationship("User", back_populates="discussions", primaryjoin= lambda: User.id==Discussion.user_id)
+    likes = db.relationship('Like', back_populates='discussion')
 
 class User (db.Model):
-    __tablename__='users'
+    __tablename__='user'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(120), nullable=False)
+
+
     observations = relationship("Observation", back_populates="user")
     discussions = relationship("Discussion", back_populates="user")
     likes = db.relationship('Like', back_populates='user')
 
 class Like (db.Model):
-    __tablename__='likes'
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-    discussion_id = db.Column(db.Integer, db.ForeignKey('discussions.id'), nullable=True)
-    observation_id = db.Column(db.Integer, db.ForeignKey('observations.id'), nullable=True)
+    __tablename__='like'
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    discussion_id = db.Column(db.Integer, db.ForeignKey('discussion.id'), nullable=True)
+    observation_id = db.Column(db.Integer, db.ForeignKey('observation.id'), nullable=True)
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     user = db.relationship('User', back_populates='likes')
     discussion = db.relationship('Discussion', back_populates='likes')
