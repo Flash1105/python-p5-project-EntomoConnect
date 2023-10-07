8c815c9523b39f1679d1301f9b5b06763aa078c2
Flash1105-2 <flashpay110599@gmail.com>
2023-10-07 00:22:37 -0400

    display dashboard

--- client/src/pages/DashboardPage.js
+++ client/src/pages/DashboardPage.js
@@ -1,12 +1,64 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const DashboardPage = () => {
-    return (
-        <div>
-            <h1>Dashboard Page</h1>
+  const [observations, setObservations] = useState([]);
+  const [discussions, setDiscussions] = useState([]);
 
-        </div>    
-    );
+  useEffect(() => {
+    fetchObservations();
+    fetchDiscussions();
+  }, []);
+
+  const fetchObservations = async () => {
+    try {
+      const response = await fetch('/api/observations');
+      if (response.ok) {
+        const data = await response.json();
+        setObservations(data);
+      } else {
+        // Handle error
+      }
+    } catch (error) {
+      console.error('Error fetching observations:', error);
+    }
+  };
+
+  const fetchDiscussions = async () => {
+    try {
+      const response = await fetch('/api/discussions');
+      if (response.ok) {
+        const data = await response.json();
+        setDiscussions(data);
+      } else {
+        // Handle error
+      }
+    } catch (error) {
+      console.error('Error fetching discussions:', error);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <div>
+        <h2>Observations.  this is where observations will be present</h2>
+        {observations.map((observation) => (
+          <div key={observation.id}>
+            <h3>{observation.title}</h3>
+            <p>{observation.content}</p>
+          </div>
+        ))}
+      </div>
+      <div>
+        <h2>Discussions.  this is where you can see comments</h2>
+        {discussions.map((discussion) => (
+          <div key={discussion.id}>
+            <p>{discussion.content}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
-export default DashboardPage
+export default DashboardPage;
