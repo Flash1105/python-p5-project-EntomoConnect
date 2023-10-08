c7c01d5745d540298141d9ad708cdb3952cbadec
Flash1105-2 <flashpay110599@gmail.com>
2023-10-07 23:05:04 -0400

    add discussions form

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
-import Dashbaord from './pages/DashboardPage';
+import Dashboard from './pages/DashboardPage';
 import RegisterPage from './pages/RegisterPage';
 import LoginPage from './pages/LoginPage';
-
+import DiscussionForm from './pages/DiscussionForm';
 
 const App = () => {
   return (
@@ -12,7 +12,8 @@ const App = () => {
       <Route path="/" exact component={HomePage} />
       <Route path="/register" component={RegisterPage} />
       <Route path="/login" component={LoginPage} />
-      <Route path="/dashboard" component={Dashbaord} />
+      <Route path="/dashboard" component={Dashboard} />
+      <Route path="/discussions-form" component={DiscussionForm} />
     </Router>
   );
 };
