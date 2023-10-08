813feb84c9d336e73fa2841c2a4d4f8eeaeb75d7
Flash1105-2 <flashpay110599@gmail.com>
2023-10-08 12:56:49 -0400

    remove unneeded aspects

--- server/models.py
+++ server/models.py
@@ -1,11 +1,8 @@
-from datetime import datetime 
+from datetime import datetime
 from extensions import db
 from sqlalchemy import ForeignKey, PrimaryKeyConstraint
 from sqlalchemy.orm import relationship, validates
 import re
-
-############
-
 
 class Observation(db.Model):
     __tablename__ = 'observation'
@@ -19,8 +16,8 @@ class Observation(db.Model):
     def validate_title_type(self, key, title):
         assert isinstance(title, str), "Title must be a string"
         return title
-    
-    user = db.relationship("User", back_populates="observations", primaryjoin= lambda: User.id == Observation.user_id)
+
+    user = db.relationship("User", back_populates="observations")
     likes = db.relationship('Like', back_populates='observation')
 
 class Discussion(db.Model):
@@ -30,45 +27,45 @@ class Discussion(db.Model):
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
     user_id = db.Column(db.Integer, ForeignKey('user.id'), nullable=True)
-    observation_id = db.Column(db.Integer, ForeignKey('observation.id'), nullable=True)
-    user = db.relationship("User", back_populates="discussions", primaryjoin= lambda: User.id==Discussion.user_id)
-    likes = db.relationship('Like', back_populates='discussion')
+    observation_id = db.Column(db.Integer, ForeignKey('observation.id'), nullable=False)
 
-class User (db.Model):
-    __tablename__='user'
+    user = db.relationship("User", back_populates="discussions")
+
+class User(db.Model):
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(120), nullable=False)
 
-    
     @validates('email')
     def validate_email_format(self, key, email):
         pattern = r"[^@]+@[^@]+\.[^@]+"
         assert re.match(pattern, email), "Invalid email format"
         return email
+
     @validates('password')
     def validate_password(self, key, password):
         assert len(password) >= 8
         return password
 
-    observations = relationship("Observation", back_populates="user")
-    discussions = relationship("Discussion", back_populates="user")
+    observations = db.relationship('Observation', back_populates='user')
+    discussions = db.relationship('Discussion', back_populates='user')
     likes = db.relationship('Like', back_populates='user')
 
-class Like (db.Model):
-    __tablename__='like'
+class Like(db.Model):
+    __tablename__ = 'like'
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    discussion_id = db.Column(db.Integer, db.ForeignKey('discussion.id'), nullable=True)
-    observation_id = db.Column(db.Integer, db.ForeignKey('observation.id'), nullable=True)
+    observation_id = db.Column(db.Integer, db.ForeignKey('observation.id'), nullable=False)
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+
     user = db.relationship('User', back_populates='likes')
-    discussion = db.relationship('Discussion', back_populates='likes')
     observation = db.relationship('Observation', back_populates='likes')
+
     __table_args__ = (
-        PrimaryKeyConstraint('user_id', 'discussion_id', 'observation_id'),
+        PrimaryKeyConstraint('user_id', 'observation_id'),
         {}
     )
 
     def __repr__(self):
-        return f'<User {self.id}>'
+        return f'<Like by User {self.user_id} on Observation {self.observation_id}>'
