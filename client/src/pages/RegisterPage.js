a315fb7ac47fef6f087c51d7b736bb33e2247190
Flash1105-2 <flashpay110599@gmail.com>
2023-10-07 23:05:40 -0400

    validate by string or integer

--- client/src/pages/RegisterPage.js
+++ client/src/pages/RegisterPage.js
@@ -7,9 +7,15 @@ const validate = values => {
     const errors = {};
     if (!values.username) {
         errors.username = 'Required';
+    } else if(typeof values.username !== 'string') {
+        errors.username = 'Must be a string'
     }
     if (!values.email) {
         errors.email = 'Required';
+    } else if (
+        !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
+    ) {
+        errors.email = 'Invalid email address';
     }
     if (!values.password) {
         errors.password = 'Required';
