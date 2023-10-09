b8897ecb66dc055e6742d680fccd3321957d5e55
Flash1105-2 <flashpay110599@gmail.com>
2023-10-09 01:41:44 -0400

    update navbar with all links

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -6,6 +6,12 @@ const NavBar = () => {
     <nav>
       <Link to="/register">Register</Link>
       <Link to="/login">Login</Link>
+      <Link to="/dashboard">Dashboard</Link>
+      <Link to="/logout">Logout</Link>
+      <Link to="/">Home</Link>
+      <Link to ="/discussions-form">Discussions</Link>
+      <Link to ="/observations-form">Observations</Link>
+      <Link to ="/edit-observation">Edit Observation</Link>
     </nav>
   );
 };
