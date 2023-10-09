28ebfebf14559d723c028037877c244101d6a8e2
Flash1105-2 <flashpay110599@gmail.com>
2023-10-08 21:34:48 -0400

    edit observation logic

--- client/src/pages/ObservationForm.js
+++ client/src/pages/ObservationForm.js
@@ -1,18 +1,33 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { useHistory } from 'react-router-dom';
-
+import React, { useState, useEffect } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 
 function ObservationForm() {
     const [title, setTitle] = useState('');
     const [content, setContent] = useState('');
     const [errorMessage, setErrorMessage] = useState('');
     const [successMessage, setSuccessMessage] = useState('');
-    
+
+    const location = useLocation();
     const history = useHistory();
+
+    const queryParams = new URLSearchParams(location.search);
+    const observationIdParam = queryParams.get('id') || '';
+    const contentParam = queryParams.get('content');
+
+    useEffect(() => {
+        if (observationIdParam) {
+            setTitle(`Edit Observation ${observationIdParam}`);
+            setContent(contentParam || '');
+        } else {
+            setTitle('');
+            setContent('');
+        }
+    }, [observationIdParam, contentParam]);
+
     const handleSubmit = async (e) => {
         e.preventDefault();
-        
+
         setErrorMessage('');
         setSuccessMessage('');
 
@@ -24,53 +39,69 @@ function ObservationForm() {
             setErrorMessage('Content is required');
             return;
         }
+
         const observationData = {
             title: title,
             content: content,
-    };
+        };
 
         try {
-            const response = await fetch('/api/observations', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(observationData),
-            });
+            let response;
 
-            if (response.status === 201) {
-                console.log('observation created successfully');    
-                setTitle('');          
+            if (observationIdParam) {
+                response = await fetch(`http://127.0.0.1:5555/api/observations/${observationIdParam}`, {
+                    method: 'PUT',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify(observationData),
+                });
+            } else {
+                response = await fetch('http://127.0.0.1:5555/api/observations', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify(observationData),
+                });
+            }
+           
+            if (response.status === 200 || response.status === 201) {
+                const message = observationIdParam ? 'Observation updated successfully' : 'Observation created successfully';
+                console.log(message);
+                setTitle('');
                 setContent('');
-                
-            const newObservation = await response.json();
+
+                const newObservation = await response.json();
                 history.push('/dashboard', { newObservation });
-            
+                setSuccessMessage(message);
             } else {
                 console.error('There was an error creating the observation');
+                setErrorMessage('There was an error creating the observation');
             }
         } catch (error) {
             console.error('There was an error', error);
+            setErrorMessage('There was an error. Please try again later.');
         }
     };
 
     return (
         <div>
-            <h2>Create a New Observation</h2>
+            <h2>{observationIdParam ? 'Edit Observation' : 'Create a New Observation'}</h2>
             <form onSubmit={handleSubmit}>
                 <div>
-                    <label html For="title">Title</label>
+                    <label htmlFor="title">Title</label>
                     <input
-                    id="title"
-                    type="text"                    
-                    value={title}
-                    onChange={(e) => setTitle(e.target.value)}
-                    placeholder="Title of observation"
-                    required
-                />
-             </div>
-            <div>    
-                <label htmlFor="content">Content</label>
+                        id="title"
+                        type="text"
+                        value={title}
+                        onChange={(e) => setTitle(e.target.value)}
+                        placeholder="Title of observation"
+                        required
+                    />
+                </div>
+                <div>
+                    <label htmlFor="content">Content</label>
                     <textarea
                         id="content"
                         value={content}
@@ -79,7 +110,7 @@ function ObservationForm() {
                         required
                     ></textarea>
                 </div>
-                <button type="submit">Submit</button>
+                <button type="submit">{observationIdParam ? 'Update' : 'Submit'}</button>
             </form>
             {errorMessage && <p className="error">{errorMessage}</p>}
             {successMessage && <p className="success">{successMessage}</p>}
